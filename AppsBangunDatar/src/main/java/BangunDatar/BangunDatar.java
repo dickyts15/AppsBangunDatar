7b35bb26e7b9547c9b8b6f01f09432d1b0cc9b7d
dickyts15 <dickytegar15@gmail.com>
2022-12-20 11:09:20 +0700

    nambahin komen

--- AppsBangunDatar/src/main/java/BangunDatar/BangunDatar.java
+++ AppsBangunDatar/src/main/java/BangunDatar/BangunDatar.java
@@ -26,6 +26,7 @@ abstract class BangunDatar {
         this.namaBangunDatar = namaBangunDatar;
     }
     
+    //Method untuk menghitung luas dan keliling bangun datar
     abstract double luas();
     
     abstract double keliling();
